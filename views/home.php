b04090cbccd22e75b60fa6c09a550cbd442feaa4
Ashish Y <ypratap510@gmail.com>
2025-07-16 20:10:14 +0530

    Docs- Some Notes on IBM IBM - Mastering the Art of Prompting

--- views/home.php
+++ views/home.php
@@ -4,3 +4,8 @@
 // Include the middle content section (specific to homepage only)
 include __DIR__ . '/../sections/middle-content.php';
 include __DIR__ . '/../sections/bottom-scrollbar.php';
+
+// $scripts = '
+//   <script src="/public/assets/js/components/section-header.js" defer></script>
+// ';
+// include __DIR__ . '/../templates/layout.php';
